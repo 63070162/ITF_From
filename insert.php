54e79d49221675c240b29fbce469ae57f28b088e
it63070113 <73680313+it63070113@users.noreply.github.com>
2020-11-18 22:14:44 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,7 +7,7 @@
 	$name = $_POST['name'];
 	$comment = $_POST['comment'];
 	$link = $_POST['link'];
-	$sql = "INSERT INTO guestbook (Name, Comment, Link) VALUES ('$name', '$comment', '$link')";
+	$sql = "INSERT INTO guestbook (name, comment, link) VALUES ('$name', '$comment', '$link')";
 ?>
 <!DOCTYPE html>
 <html>
