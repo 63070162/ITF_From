14279c5ebe69f2b85ec66f4edee68aa7b8e6f70b
it63070113 <73680313+it63070113@users.noreply.github.com>
2020-11-19 14:51:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Database | 13th ITF LAB</title>
+	<title>Show Data</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"">
 	<style type="text/css">
 		.card {
-			border-top: solid 5px #ffc107;
+			border-top: solid 5px #888888;
 		}
 	</style>
 </head>
@@ -30,7 +30,7 @@
 								}
 								$res = mysqli_query($conn, 'SELECT * FROM guestbook');
 							?>
-							<table class="table table-borderless table-hover">
+							<table class="table thead-inverse">
 								<thead>
 									<tr>
 										<th width="150px">Action</th>
